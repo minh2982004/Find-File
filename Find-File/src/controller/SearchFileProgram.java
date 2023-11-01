b4df0219bc1bba797d4332b82c7a6b68537446e0
minh2982004 <144196654+minh2982004@users.noreply.github.com>
2023-11-01 14:18:30 +0700

    Add files via upload

---
+++ Find-File/src/controller/SearchFileProgram.java
@@ -0,0 +1,29 @@
+
+package controller;
+
+import view.Menu;
+import common.Algorithm;
+
+public class SearchFileProgram extends Menu<String> {
+    static String[] options = {"Count Word In File", "Find File By Word", "Exit"};
+    Algorithm ar;
+    public SearchFileProgram() {
+        super("========== Word Program ==========", options);
+        ar = new Algorithm();
+    }
+
+    @Override
+    public void execute(int n) {
+        switch(n) {
+            case 1: ar.countWordInFile();
+            break;
+            case 2: ar.findFileByWord();
+            break;
+            case 3: System.exit(0);
+            break;
+        }
+    }
+
+  
+    
+}
---
+++ Find-File/src/main/Main.java
@@ -0,0 +1,12 @@
+
+package main;
+
+import controller.SearchFileProgram;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new SearchFileProgram().run();
+    }
+    
+}
